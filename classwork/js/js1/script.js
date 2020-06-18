04e61dfaed71c8bd6e7073f28a93ec34a50dc8da
Paulius99 <mail.pauliusg@gmail.com>
2020-06-18 11:21:29 +0300

    nauji

---
+++ classwork/js/js1/script.js
@@ -0,0 +1,11 @@
+var answerToPrompt = prompt("what is your car model?");
+var answerTocolor = prompt("what is your car model color?");
+
+if ((answersToPrompt = null)) {
+  answersToPrompt = "errow";
+}
+
+var answersToPrompt =
+  '<p style=" color: red;">' + answerToPrompt + " " + answerTocolor;
+
+document.write(answersToPrompt);
